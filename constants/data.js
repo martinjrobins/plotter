d89d8caa6c3238bd3bea01ae5edae2c8b7e47a9c
Martin Robinson <martinjrobins@gmail.com>
2020-08-26 17:23:16 +0100

    change order of csvFiles

--- constants/data.js
+++ constants/data.js
@@ -1,11 +1,11 @@
 export const csvFiles = [
   {
-    url: 'reduced_output_timestep_2015.csv',
-    description: 'Nismod output for Local Area Districts (daily data for 2015)',
-  },
-  {
     url: 'https://vega.github.io/vega-lite/data/seattle-weather.csv',
     description: 'Seattle weather',
+  },
+  {
+    url: 'reduced_output_timestep_2015.csv',
+    description: 'Nismod output for Local Area Districts (daily data for 2015)',
   },
 ]
 
